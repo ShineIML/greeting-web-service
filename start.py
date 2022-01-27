35837b128a507ac4bc2735f6be32e801bbdb6631
Shinezxc <nerokyqq@gmail.com>
2022-01-27 11:20:24 +0100

    adding project

---
+++ start.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, redirect, session, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pguser:pgpassword@localhost:5433/app_db'
+db = SQLAlchemy(app)
+
+
+class Guest(db.Model):
+    username = db.Column(db.String(80), primary_key=True, unique=True, nullable=False)
+
+    def __repr__(self):
+        return self.username
+
+
+db.create_all()
+
+text = None
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/name', methods=['GET', 'POST'])
+def add_name():
+    try:
+        user = Guest(username=request.form['name'])
+        db.session.add(user)
+        db.session.flush()
+        db.session.commit()
+        return render_template('greetings.html', user=user)
+    except:
+        db.session.rollback()
+        return render_template('seen.html', user=user)
+
+
+
+@app.route('/collection', methods=['GET'])
+def collection():
+    return render_template('collection.html', collection=Guest.query.all())

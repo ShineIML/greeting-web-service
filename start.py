9b1e0ee5f3f2ef4e0d74ae4fba1cb6346827ece2
Shinezxc <nerokyqq@gmail.com>
2022-01-29 13:24:16 +0100

    start.py

--- start.py
+++ start.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, session, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_pyfile('settings.py')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jbduzvmgcsebtb:27f5e32b0cebfa3ca9171208a59eda40e00cd70a99d810eccf30c61a66b526ba@ec2-54-73-152-36.eu-west-1.compute.amazonaws.com:5432/dasvmqpo1ebrjj'
 db = SQLAlchemy(app)
 
 #postgresql://pguser:pgpassword@localhost:5433/app_db
@@ -15,8 +15,6 @@ class Guest(db.Model):
 
 db.create_all()
 
-text = None 
-
 
 @app.route('/', methods=['GET'])
 def index():
@@ -28,6 +26,7 @@ def add_name():
     try:
         user = Guest(username=request.form['name'])
         db.session.add(user)
+        db.session.flush()
         db.session.commit()
         return render_template('greetings.html', user=user)
     except:

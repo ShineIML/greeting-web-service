2c5e4f13acc43b323d4106a722749ea3ddac30e7
Shinezxc <nerokyqq@gmail.com>
2022-01-29 15:12:18 +0100

    upd style

--- start.py
+++ start.py
@@ -38,5 +38,5 @@ def add_name():
 
 @app.route('/collection', methods=['GET'])
 def collection():
-    collection = Guest.query.all()
+    collection = Guest.query.all() 
     return render_template('collection.html', collection=collection)

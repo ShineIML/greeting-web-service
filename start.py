18c4967462479d8ee6ab0169a04ad927ddec5dec
Shinezxc <nerokyqq@gmail.com>
2022-01-29 13:45:08 +0100

    upd

--- start.py
+++ start.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, redirect, session, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+from settings import SQLALCHEMY_DATABASE_URI
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://xdfhkkmhunxxim:5b2f2c590b5963a8bd3422bce0283a7730a9b0dea7ee2a7580e15ee291f7e8d6@ec2-34-242-89-204.eu-west-1.compute.amazonaws.com:5432/d1r7qn9gh3f6ug'
+app.config[SQLALCHEMY_DATABASE_URI] = 'postgresql://xdfhkkmhunxxim:5b2f2c590b5963a8bd3422bce0283a7730a9b0dea7ee2a7580e15ee291f7e8d6@ec2-34-242-89-204.eu-west-1.compute.amazonaws.com:5432/d1r7qn9gh3f6ug'
 db = SQLAlchemy(app)
 
 #postgresql://pguser:pgpassword@localhost:5433/app_db
+
 class Guest(db.Model):
     username = db.Column(db.String(80), primary_key=True, unique=True, nullable=False)
 
@@ -26,7 +28,6 @@ def add_name():
     try:
         user = Guest(username=request.form['name'])
         db.session.add(user)
-        db.session.flush()
         db.session.commit()
         return render_template('greetings.html', user=user)
     except:

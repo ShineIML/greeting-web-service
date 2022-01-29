2aae991961cd5211404f82bacbf836aa6751e119
Shinezxc <nerokyqq@gmail.com>
2022-01-29 15:04:37 +0100

    upd style

--- start.py
+++ start.py
@@ -27,6 +27,7 @@ def add_name():
     try:
         user = Guest(username=request.form['name'])
         db.session.add(user)
+        db.session.flush()
         db.session.commit()
         return render_template('greetings.html', user=user)
     except:
